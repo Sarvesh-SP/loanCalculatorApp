e05d455e519300a7a9830868a683800fb144fe98
Sarvesh-SP <sarvesh.sp2013@gmail.com>
2021-04-21 09:38:29 +0530

    Added graph function

--- loanFunc.py
+++ loanFunc.py
@@ -1,3 +1,4 @@
+from matplotlib import pyplot
 def get_loan_info():
   loan = {}
   loan['principal'] = float(input('Enter the loan amount: '))
@@ -25,9 +26,33 @@ def make_monthly_payment(loan):
     loan['money paid'] += loan['monthly payment'] + loan['principal']
     loan['principal'] = 0
 
-def summarize():
-  pass
+def summarize(loan, num, initial_principal):
+  print(f'\nCongratulations! You paid off your loan in {num}.')
+  print(f'Your initial loan was ${initial_principal}  at a rate of {loan['rate']*100}%.')
+  print(f'Your monthly payment was ${loan['monhtly payment'.]}')
+  print(f'You spent ${round(loan['money paid'], 2)} in total.')
 
+  #Calculate money spent on interest
+  interest = round(loan['money paid'] - initial_principal, 2)
+  print("You spent $" + str(interest) + " on interest!")
 
-def create_graph():
-  pass
+def create_graph(data, loan):
+  """Create a graph to show the relationship between principal and time"""
+  
+  x_values = [] #These represent month numbers
+  y_values = [] #These represent corresponding principal values
+
+ #Loop through data set. Point is a tuple. #point[0] represents a month number, point[1] represents a principal value.
+for point in data:
+  x_values.append(point[0])
+  y_values.append(point[1])
+
+#Create a plot for x_values and y_values (month number and principal)
+  pyplot.plot(x_values, y_values)
+  pyplot.title(str(100*loan['rate']) + "% Interest With $" + str(loan['monthly
+payment']) + " Monthly Payment")
+  pyplot.xlabel("Month Number")
+  pyplot.ylabel("Principal of Loan")
+
+  #Display the created graph
+  pyplot.show()
